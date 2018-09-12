62ca2b315e5c6add8a8d7fc5e0438da94bab0e93
Jason <jasonsmorrow@gmail.com>
2018-09-11 23:24:46 -0700

    Updated to format output message.  Give F, C, and current conditions (first in list)

--- weather.js
+++ weather.js
@@ -5,6 +5,16 @@ const KEY = apikey.APIKEY;
 // Require http to access Open Weather Map
 http = require('http');
 
+function toFaren(temp){
+  fTemp = (9/5) * (parseFloat(temp) - 273) + 32;
+  return fTemp.toFixed(1).toString();
+}
+
+function toCels(temp){
+  cTemp = parseFloat(temp) - 273;
+  return cTemp.toFixed(1).toString();
+}
+
 function get(query) {
   const request =
   http.get(`http://api.openweathermap.org/data/2.5/weather?${query}&APPID=${KEY}`, response => {
@@ -17,7 +27,8 @@ function get(query) {
 
     response.on('end', () => {
       const weather = JSON.parse(currentWeather);
-      console.log(weather);
+      let message = `Current weather in ${weather.name} is ${toFaren(weather.main.temp)}F (${toCels(weather.main.temp)}C) with ${weather.weather[0].description}.`
+      console.log(message);
     });
   });
 }
